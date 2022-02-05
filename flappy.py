472fa98caeb24e40ef05e7a83c1485ab248f6ce9
01Gautami <gautamip21ite@student.mes.ac.in>
2022-02-05 13:43:15 +0530

    first commit

---
+++ flappy.py
@@ -0,0 +1,30 @@
+from unicodedata import name
+
+
+print('HELLO WORLD')
+
+print(1+1)
+
+print(2*2)
+
+print(1/2)
+
+print(2**4)
+
+#varibles
+name="gautami"
+print(name)
+
+x=4
+y=9
+z=x+y
+print(z)
+
+# strings
+print('in single quotes')
+print("in double quotes")
+print("And os it is")
+
+favourite_number = 4
+print('My name is' +name+ 'and my favourites number is' + str(favourite_number))
+print(f'My name is {name} and my favourites number is {favourite_number}')
